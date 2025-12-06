5182d1c14d2f6f9cf217edab992d78560b2ea447
luisdrz20 <luisdavidrs002@gmail.com>
2025-12-06 13:24:15 -0600

    actualiacion2

--- DJMENSAJES/settings.py
+++ DJMENSAJES/settings.py
@@ -81,12 +81,7 @@ CHANNEL_LAYERS = {
 }
 
 DATABASES = {
-
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
